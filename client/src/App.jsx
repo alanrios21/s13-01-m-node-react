0d7d514ac4922829d0ad2bd86689881e1c0c625d
AndresMejia <anmepe.sud@gmail.com>
2024-02-20 11:10:06 +1030

    trabajand en carga videos

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,15 +2,17 @@ import "./App.css";
 import { LoginPage } from "./pages/auth/LoginPage";
 import { RegisterPage } from "./pages/auth/RegisterPage";
 import Navbar from "./components/Navbar";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Routes, Route } from "react-router-dom";
 import MyMusic from "./pages/Music/MyMusic";
+import Videos from './pages/Videos/Videos';
 
 function App() {
   return (
     <>
         <Navbar />
-        <MyMusic />
         <Routes>
+        <Route path="/videos" element={<Videos />} />
+        <Route path="/music" element={<MyMusic />} />
           <Route path="/auth">
             <Route path="login" element={<LoginPage />} />
             <Route path="register" element={<RegisterPage />} />
