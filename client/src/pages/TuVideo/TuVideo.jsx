c206e0d3f13cc92322d776f97430bb2930834208
Alan Rios <riosalan264@gmail.com>
2024-02-21 19:10:33 -0300

    Vista de Tus videos en responsive y Dekstop

--- client/src/pages/TuVideo/TuVideo.jsx
+++ client/src/pages/TuVideo/TuVideo.jsx
@@ -1,22 +1,23 @@
 
-import ImgMusic from '../../assets/imgMusic.png'
+import ImgMusic from '../../assets/imgVideo.png'
 import PlayCircleIcon from '@mui/icons-material/PlayCircle';
+import './TuVideo.css'
 
 const TuVideo = () => {
   return (
     <div>
-      <div className="contenedor-music">
-       <div className='title-music'>
+      <div className="contenedor-video">
+       <div className='title-video'>
           <h1>Tus videos</h1>
        </div>
-       <div className='subtitle-music'>
-          <h1>Demos</h1>
+       <div className='subtitle-video'>
+          <h1>Videos</h1>
        </div>
        <div className='img-container'>
           <img src={ImgMusic}></img>
        </div>
        <div className='p-container'>
-          <p><PlayCircleIcon className='icon-music'></PlayCircleIcon>225 Reproducciones</p>
+          <p><PlayCircleIcon className='icon-video'></PlayCircleIcon>106 Reproducciones</p>
        </div>
     </div>
     </div>
