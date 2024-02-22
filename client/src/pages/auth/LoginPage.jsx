2102eb1b8937ee46788d009b07609358245db2d2
MayderC <maydercortes1@gmail.com>
2024-02-22 02:56:55 -0600

    Update height of login and register sections

--- client/src/pages/auth/LoginPage.jsx
+++ client/src/pages/auth/LoginPage.jsx
@@ -5,7 +5,7 @@ export const LoginPage = () => {
   return (
     <>
       <main className="w-full h-screen bg-primary grid place-items-center">
-        <section className="h-[70vh]">
+        <section className="lg:h-[75vh]">
           <LoginForm />
         </section>
       </main>
--- client/src/pages/auth/RegisterPage.jsx
+++ client/src/pages/auth/RegisterPage.jsx
@@ -3,8 +3,10 @@ import "./styles.css";
 
 export const RegisterPage = () => {
   return (
-    <main className="w-full h-screen bg-primary grid place-items-center">
-      <RegisterForm />
+    <main className="w-full min-h-screen bg-primary grid place-items-center">
+      <section className="lg:h-[75vh]">
+        <RegisterForm />
+      </section>
     </main>
   );
 };
