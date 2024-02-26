82788c2fe2ef36a9938f519c69f0e1ee0e2bffc7
AndresMejia <anmepe.sud@gmail.com>
2024-02-26 19:55:01 +1030

    video multimedia

--- client/src/components/VideosForm/VideosForm.jsx
+++ client/src/components/VideosForm/VideosForm.jsx
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import cargaVideos from '../../assets/cargaVideos.png';
+import imgVideo1 from '../../assets/imgVideo1.png';
+import imgVideo2 from '../../assets/imgVideo2.png';
+import imgVideo3 from '../../assets/imgVideo3.png';
+import imgVideo4 from '../../assets/imgVideo4.png';
 import { httpInstance } from '../../api/httpInstance';
 
 const VideosForm = () => {
@@ -33,14 +37,22 @@ const VideosForm = () => {
   };
 
   return (
-    <div className='ml-2 p-2'>
+    <div className='ml-2 p-2 '>
       <h1 className='text-xl font-bold mt-2'>Contenido multimedia</h1>
-      <h2 className='font-bold mt-4'>Videos</h2>
+      <h2 className='text-xl mt-4'>Videos</h2>
       <p className='mt-2'>Cada video es una oportunidad para contar tu historia de una manera visualmente emocionante. 
         Sigue compartiéndolos y lleva tu música a nuevos niveles de creatividad y expresión</p>
-      <label htmlFor="file-upload">
-        <img src={cargaVideos} alt="Cargar Video"/>
-      </label>
+      <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 lg:grid-cols-3 xl:grid-cols-3 gap-4 mb-8 mt-8">
+       <img src={imgVideo1} alt="Video1" className="w-full h-auto rounded-xl" />
+       <img src={imgVideo2} alt="Video2" className="w-full h-auto rounded-xl" />
+       <img src={imgVideo3} alt="Video3" className="w-full h-auto rounded-xl" />
+       <img src={imgVideo4} alt="Video4" className="w-full h-auto rounded-xl" />
+       <div className="flex justify-center mb-4 mt-4">
+        <label htmlFor="file-upload">
+          <img src={cargaVideos} alt="Cargar Video" />
+        </label>
+      </div>
+      </div>  
       <input
         id="file-upload"
         type="file"
