ee0b36e9ca3030a767e19fe5941fa26a370fb3af
Lalomax <lalomax365@gmail.com>
2024-03-03 18:49:00 -0400

    Responsiveness added

--- client/src/components/Footer/Footer.jsx
+++ client/src/components/Footer/Footer.jsx
@@ -2,11 +2,11 @@ import React, { useState } from "react";
 import LogoSingle from "../../assets/logo-single.png";
 import Instagram from "../../assets/instagram.svg";
 import Facebook from "../../assets/facebook.svg";
-import Add from "../../assets/add.svg"
+import Add from "../../assets/add.svg";
 
 const Footer = () => {
-  const [buttonState1, setButtonState1] = useState(false)
-  const [buttonState2, setButtonState2] = useState(false)
+  const [buttonState1, setButtonState1] = useState(false);
+  const [buttonState2, setButtonState2] = useState(false);
   return (
     <footer className="bg-primary">
       <div className="mx-auto w-full max-w-screen-xl p-4 py-6 lg:py-8">
@@ -25,35 +25,70 @@ const Footer = () => {
             </a>
           </div>
           {/* Footer Items Container */}
-          <div className="grid grid-cols-1 md:grid-cols-2 gap-4 ">
-            
+          <div className="grid grid-cols-1 md:grid-cols-2 md:gap-4 ">
             {/* First item */}
-            
-            <div>
-              <h2 className="mb-6 text-sm font-semibold text-white">
-                Sobre nosotros <button><img src={Add} alt="my image" onClick={() => setButtonState1(!buttonState1)} /></button>
-              </h2>
-              <ul className={`text-gray-500 ${buttonState1 ? `block` : `hidden`}`}>
-                <li className="mb-4">
+
+            <div className="mx-12 md:mx-0 md:text-center text-left">
+              {/* drop-down button */}
+
+              <div className="flex flex-row justify-between md:justify-center ">
+                <h2 className="mb-6 text-sm font-semibold text-white">
+                  Sobre nosotros
+                </h2>
+
+                <div>
+                  <button className="md:hidden">
+                    <img
+                      src={Add}
+                      alt="my image"
+                      onClick={() => setButtonState1(!buttonState1)}
+                    />
+                  </button>
+                </div>
+              </div>
+
+              <div
+                className={`text-gray-500 md:block ${
+                  buttonState1 ? `block` : `hidden`
+                }`}
+              >
+                <div className="mb-4">
                   <a href="#" className="hover:underline">
                     Historia, Misión y Visión
                   </a>
-                </li>
-                <li>
+                </div>
+                <div>
                   <a href="#" className="hover:underline">
                     Contacto
                   </a>
-                </li>
-              </ul>
+                </div>
+              </div>
             </div>
-            
+
             {/* Second Item */}
-            
-            <div>
-              <h2 className="mb-6 text-sm font-semibold  text-white">
-                Términos y Privacidad <button><img src={Add} alt="my image" onClick={() => setButtonState2(!buttonState2)} /></button>
-              </h2>
-              <ul className={`text-gray-500 ${buttonState2 ? `block` : `hidden`}`}>
+
+            <div className="mx-12 md:mx-0 md:text-center text-left">
+              {/* drop-down button */}
+
+              <div className="flex flex-row justify-between md:justify-center ">
+                <h2 className="mb-6 text-sm font-semibold text-white">
+                  Terminos y privacidad
+                </h2>
+                <div>
+                  <button className="md:hidden">
+                    <img
+                      src={Add}
+                      alt="my image"
+                      onClick={() => setButtonState2(!buttonState2)}
+                    />
+                  </button>
+                </div>
+              </div>
+              <ul
+                className={`text-gray-500 md:block ${
+                  buttonState2 ? `block` : `hidden`
+                }`}
+              >
                 <li className="mb-4">
                   <a href="#" className="hover:underline ">
                     Normas de la comunidad
@@ -68,11 +103,11 @@ const Footer = () => {
             </div>
           </div>
           {/* Social networks */}
-          <div>
+          <div className="ml-12 md:ml-0">
             <h2 className="mb-6 text-sm font-semibold text-white">
               Nuestras redes sociales
             </h2>
-            <div className="flex flex-row justify-around ">
+            <div className="flex flex-row gap-4 md:justify-center">
               <div className="">
                 <a href="#" className="hover:underline">
                   <img src={Instagram} alt="" />
--- client/src/pages/Inicio/Inicio.jsx
+++ client/src/pages/Inicio/Inicio.jsx
@@ -1,15 +1,16 @@
-import React from 'react';
-import InicioForm from '../../components/Inicio/InicioForm';
-import Footer from "../../components/Footer/Footer"
-
+import React from "react";
+import InicioForm from "../../components/Inicio/InicioForm";
+import Footer from "../../components/Footer/Footer";
 
 const Inicio = () => {
   return (
-    <div >
-        <InicioForm />
+    <div>
+      <InicioForm />
+      <div className="">
         <Footer />
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Inicio
+export default Inicio;
