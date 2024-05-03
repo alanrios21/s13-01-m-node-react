7e1d258dcea0b4fbb2660369cb978defdbb241d5
Mayder C <maydercortes1@gmail.com>
2024-05-02 23:59:43 -0700

    chore: Update package.json scripts for development and production environments

--- server/src/app.js
+++ server/src/app.js
@@ -1,41 +1,45 @@
 const express = require("express");
-const connectMongoDBSession = require('connect-mongodb-session')
-const session = require('express-session');
+const connectMongoDBSession = require("connect-mongodb-session");
+const session = require("express-session");
 const morgan = require("morgan");
 const routes = require("../src/routes/index");
 const cors = require("cors");
-const swaggerUi = require('swagger-ui-express');
-const swaggerSpec = require('./utils/swagger/swagger');
+const swaggerUi = require("swagger-ui-express");
+const swaggerSpec = require("./utils/swagger/swagger");
 
 const MongoDBStore = connectMongoDBSession(session);
 
-
 const server = express();
+
+const whitelist = process.env.WHITELIST ? process.env.WHITELIST.split(",") : [];
+
+server.use(
+  cors({
+    origin: whitelist,
+  })
+);
 server.name = "RoundPeople API";
 
 // Configurar Swagger UI
-server.use('/swagger', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
+server.use("/swagger", swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 server.use(express.json());
-server.use(session({
-    secret: 'your-secret-key',
+server.use(
+  session({
+    secret: "X-User-ID",
     resave: false,
     saveUninitialized: false,
     cookie: {
-        maxAge: 3600000,
+      maxAge: 3600000,
     },
     store: new MongoDBStore({
-        collection: 'Session',
-        uri: process.env.DATABASE_URL,
+      collection: "Session",
+      uri: process.env.DATABASE_URL,
     }),
-}));
+  })
+);
 
-server.use(cors());
 server.use(morgan("dev"));
 
 server.use("/", routes);
 
-
-
-
 module.exports = server;
-
